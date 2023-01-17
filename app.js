167be663f3dc09e52623e52e51aef4faedf74079
Kartikye Mittal <kartikye@dots.dev>
2023-01-17 19:17:47 +0800

    first

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from 'express';
+const app = express();
+import path from 'path';
+import open from 'open';
+import fetch from 'node-fetch';
+
+const __dirname = path.resolve();
+
+const port = process.env.port || 5000;
+
+const dotsClientId = 'pk_prod_oonhkqNml1wWRIQa4fxbEQFClNmty';
+const dotsSecretKey = 'sk_prod_7AtJHO9Kc7hzf77XMCLDw3hsAPdr9';
+const dotsAPIUrl = 'http://127.0.0.1:8080/api/v2/payments/payment-intents';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/dots.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dots.js'));
+});
+
+app.post('/create-payment-intent', (req, res) => {
+  const amount = req.body.amount;
+
+  fetch(dotsAPIUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${Buffer.from(
+        `${dotsClientId}:${dotsSecretKey}`
+      ).toString('base64')}`,
+    },
+    body: JSON.stringify({
+      amount: amount,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ client_secret: data.client_secret });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  open(`http://localhost:${port}`);
+});
