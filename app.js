45aed9cb5eb40fc5b7c64b3cfecabbaf4baa3da4
Kartikye Mittal <kartikye@getathelas.com>
2023-02-10 16:57:41 -0800

    update to staging

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import path from 'path';
-import open from 'open';
-import fetch from 'node-fetch';
+import path from "path";
+import open from "open";
+import fetch from "node-fetch";
 
 const __dirname = path.resolve();
 
 const port = process.env.port || 5000;
 
-const dotsClientId = 'YOUR_CLIENT_ID';
-const dotsSecretKey = 'YOUR_SECRET_KEY';
-const dotsAPIUrl = 'https://pls.senddotssandbox.com/api'; // 'http://127.0.0.1:8080/api' | 'https://api.dots.dev/api'
+const dotsClientId = "YOUR_CLIENT_ID";
+const dotsSecretKey = "YOUR_SECRET_KEY";
+const dotsAPIUrl = "https://api-staging.dots.dev/api"; // 'https://pls.senddotssandbox.com/api'; // 'http://127.0.0.1:8080/api' | 'https://api.dots.dev/api'
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.post('/create-payment-intent', (req, res) => {
+app.post("/create-payment-intent", (req, res) => {
   const amount = req.body.amount;
 
-  fetch(dotsAPIUrl + '/v2/payments/payment-intents', {
-    method: 'POST',
+  fetch(dotsAPIUrl + "/v2/payment-intents", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Basic ${Buffer.from(
         `${dotsClientId}:${dotsSecretKey}`
-      ).toString('base64')}`,
+      ).toString("base64")}`,
     },
     body: JSON.stringify({
       amount: amount,
     }),
   })
-    .then((response) => response.json())
+    .then((response) => {
+      return response.json();
+    })
     .then((data) => {
       res.json({ client_secret: data.client_secret });
     });
